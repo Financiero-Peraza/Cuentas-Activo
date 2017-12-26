c5615415eea9561b4a38fdd7e6bb1fe8a56d9ac1
bortan01 <brmiranda009@gmail.com>
2017-12-26 16:08:28 -0600

    ya sirve el lateral

--- cuenta_cobrar/registro_natura.php
+++ cuenta_cobrar/registro_natura.php
@@ -3,111 +3,100 @@ include_once '../plantilla/cabecera.php';
 include_once '../plantilla/barraSuperior.php';
 include_once '../plantilla/barra_lateral_usuario.php';
 ?>
-<form action="" method="GET">
-    <!--    INICIO DE DATOS-->
+<form action="">
     <section class="content">
-          <!--INICIO DE REGISTRO-->
-            <div class="container-fluid">
-                <!-- Basic Validation -->
-                <div class="row clearfix">
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <div class="card">
-                            <div class="header">
-                                <h2 class="text-center">REGISTRO DE CLIENTE</h2>
+        <div class="container-fluid">
+            <!-- Vertical Layout -->
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+
+                        <div class="header">
+                            <h2 class="text-center">REGISTRO DE CLIENTE</h2>
+                        </div>
+                        <div class="body">
+                            <div class="row clearfix">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <div class="form-line">
+                                            <input type="text"class="form-control text-center" name="nameNombre" placeholder="NOMBRE...">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <div class="form-line">
+                                            <input type="text" class="form-control text-center" name="nameApellido" placeholder="APELLIDO...">
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="body">
-                                <div class="row clearfix">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <div class="form-line">
-                                                <input type="text"class="form-control text-center" name="nameNombre" placeholder="NOMBRE...">
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <div class="form-line">
-                                                <input type="text" class="form-control text-center" name="nameApellido" placeholder="APELLIDO...">
-                                            </div>
+                            <div class="row clearfix">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <div class="form-line">
+                                            <input type="text"class="form-control text-center" name="nameNombre" placeholder="NOMBRE...">
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="row clearfix">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <div class="form-line">
-                                                <input type="text" class="form-control text-center" name="NameDireccion" placeholder="DIRECCION...">
-                                            </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <div class="form-line">
+                                            <input type="text" class="form-control text-center" name="nameApellido" placeholder="APELLIDO...">
                                         </div>
                                     </div>
+                                </div>
+                            </div>
+                            <div class="row clearfix">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <div class="form-line">
+                                            <input type="text" class="form-control text-center" name="NameDireccion" placeholder="DIRECCION...">
+                                        </div>
+                                    </div>
+                                </div>
 
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <div class="form-line">
-                                                <input type="text" class="form-control text-center" name="NameTelefono" placeholder="TELEFONO...">
-                                            </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <div class="form-line">
+                                            <input type="text" class="form-control text-center" name="NameTelefono" placeholder="TELEFONO...">
                                         </div>
                                     </div>
                                 </div>
-                                <div class="row clearfix">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <div class="form-line">
-                                                <input type="text" class="form-control text-center" name="NameDui" placeholder="DUI...">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <div class="form-line">
-                                                <input type="text" class="form-control text-center" name="NameNit" placeholder="NIT...">
-                                            </div>
+                            </div>
+                            <div class="row clearfix">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <div class="form-line">
+                                            <input type="text" class="form-control text-center" name="NameDui" placeholder="DUI...">
                                         </div>
                                     </div>
                                 </div>
-                                <div class="row clearfix">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <div class="form-line">
-                                                <input type="number" class="form-control text-center" name="NameIngresos" placeholder="MONTO DE INGRESOS...">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <div class="form-line">
-                                                <select class="form-control show-tick">
-                                                    <option value="">TIPO DE INGRESO</option>
-                                                <option value="ASALARIADO">ASALARIADO</option>
-                                                <option value="SERVICIOS">POR SERVICIOS PROFECIONALES</option>
-                                                <option value="PROFECIONAL">PROFECIONAL INDEPENDIENTE</option>
-                                                
-                                            </select>
-                                            </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <div class="form-line">
+                                            <input type="text" class="form-control text-center" name="NameNit" placeholder="NIT...">
                                         </div>
                                     </div>
                                 </div>
-                                <div class="row clearfix">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <div class="form-line">
-                                                <input type="email" class="form-control text-center" name="NameEmail" placeholder="EMAIL...">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="text-center">
+                            </div>
+                            <div class="text-center">
                                     <button type="submit" class="btn btn-primary m-t-15 waves-effect">GUARDAR</button>
                                 </div>
-                            </div>
                         </div>
+                        
                     </div>
                 </div>
-
             </div>
-            <!--FIN DE REGISTRO!-->
+            <!-- #END# Vertical Layout -->
+                     
+           
+            </div>
+            <!-- #END# Inline Layout -->
+           
+        </div>
     </section>
 </form>
 <?php
