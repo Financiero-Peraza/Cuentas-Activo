e54af468113d3195be8502800f9957d2f0282d17
bortan01 <brmiranda009@gmail.com>
2018-01-07 00:25:16 -0600

    agregando login

--- index.php
+++ index.php
@@ -1,187 +1,170 @@
-﻿<!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <title>Casa de Encuentro Juevenil, Verapaz San Vicente</title>
-    <!-- Favicon-->
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
 
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+    <head>
+        <meta charset="UTF-8">
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-    <!-- Bootstrap Core Css -->
-    <link href="plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
 
-    <!-- Waves Effect Css -->
-    <link href="plugins/node-waves/waves.css" rel="stylesheet" />
+        <!-- Favicon-->
+        <link rel="icon" href="../favicon.ico" type="image/x-icon">
 
-    <!-- Animation Css -->
-    <link href="plugins/animate-css/animate.css" rel="stylesheet" />
+        <link href="css/jquery.dataTables.min.css" rel="stylesheet">
 
-    <!-- Morris Chart Css-->
-    <link href="plugins/morrisjs/morris.css" rel="stylesheet" />
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 
-    <!-- Custom Css -->
-    <link href="css/style.css" rel="stylesheet">
+        <!-- Bootstrap Core Css -->
+        <link href="plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
 
-    <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
-    <link href="css/themes/all-themes.css" rel="stylesheet" />
-</head>
+        <!-- Waves Effect Css -->
+        <link href="plugins/node-waves/waves.css" rel="stylesheet" />
 
-<body class="theme-red">
-    
-    
-    <!-- Top Bar -->
-    <nav class="navbar">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a href="#" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
-                <a href="#" class="bars"></a>
-                <a class="navbar-brand" href="#">Casa de Encuentro Juevenil</a>
-            </div>
-            <div class="collapse navbar-collapse" id="navbar-collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <a class="navbar-brand" href="#">San José Verapaz</a>
-                   
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!-- #Top Bar -->
-    
-    <section>
-    <!-- Left Sidebar -->
-    <aside id="leftsidebar" class="sidebar">
-        <div class="user-info">
-            <div class="image">
-                <!--<img src="images/user.png" width="48" height="48" alt="User" />-->
-            </div>
-            <div class="info-container">
-                <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></div>
-                <div class="email"></div>
-                <div class="btn-group user-helper-dropdown">
-                    <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"></i>
-                    <ul class="dropdown-menu pull-right">
+        <!-- Animation Css -->
+        <link href="plugins/animate-css/animate.css" rel="stylesheet" />
 
-                    </ul>
+        <!-- Custom Css -->
+        <link href="css/style.css" rel="stylesheet">
+
+        <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
+        <link href="css/themes/all-themes.css" rel="stylesheet" />
+
+        <!--<script type="text/javascript" src="js/jquery.min.js"></script>-->
+        <script src="plugins/jquery/jquery.min.js"></script>
+        <!--<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>-->
+
+
+
+        <link href="css/sweetalert.css" rel="stylesheet">
+        <script src="js/sweetalert.min.js"></script>
+
+    </head>
+    <body class="login-page">
+
+
+        <?php
+        if (isset($_REQUEST['nameEnviar'])) {
+            include_once 'app/Conexion.php';
+            include_once 'modelos/usuario.php';
+            include_once 'repositorios/repositorio_usuario.php';
+
+
+            $usuario = $_REQUEST['nameUsuario'];
+            $pass = $_REQUEST['namePass'];
+            Conexion::abrir_conexion();
+            $respuesta = repositorio_usuario::verificar_pass(Conexion::obtener_conexion(), $pass, $usuario);
+
+            if ($respuesta == '1') {
+                echo '<script>swal({
+                    title: "Exito",
+                     text: "Sesión iniciada correctamente",
+                    type: "success",
+                    confirmButtonText: "ok",
+                    closeOnConfirm: false
+                },
+                function () {
+                    location.href="./home/home_administrador.php";
+                    
+                });</script>';
+            } else if ($respuesta == '2') {
+
+                echo '<script>swal({
+                    title: "Exito",
+                     text: "Datos incorrectos",
+                    type: "error",
+                    confirmButtonText: "ok",
+                    closeOnConfirm: false
+                },
+                function () {
+                    location.href="./home/home_usuario.php";
+                    
+                });</script>';
+            } else {
+                echo '<script>swal({
+                    title: "Exito",
+                     text: "Datos incorrectos",
+                    type: "error",
+                    confirmButtonText: "ok",
+                    closeOnConfirm: false
+                },
+                function () {
+                    location.href="index.php";
+                    
+                });</script>';
+            }
+        } else {
+            ?>
+
+
+            <div class="login-box">
+                <div class="logo">
+                    <a href="javascript:void(0);"><b>SISTEMA DE ACTIVO FIJO Y CUENTAS POR COBRAR</b></a>
+                    <small>© 2018 UES FMP.</small>
+                </div>
+                <div class="card">
+                    <div class="body">
+                        <form id="FORMULARIO" method="get" name="" autocomplete="off">
+                            <div class="msg">Inicia tu sesión para continuar</div>
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">person</i>
+                                </span>
+                                <div class="form-line">
+                                    <input type="text" class="form-control" name="nameUsuario" placeholder="Usuario" required autofocus>
+                                </div>
+                            </div>
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">lock</i>
+                                </span>
+                                <div class="form-line">
+                                    <input type="password" class="form-control" name="namePass" placeholder="Cotraseña" required>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-8 p-t-5">
+
+                                </div>
+                                <div class="col-xs-4">
+                                    <button class="btn btn-block bg-pink waves-effect" type="submit" name="nameEnviar" value="ok">Entrar</button>
+                                </div>
+                            </div>
+                            <div class="row m-t-15 m-b--20">
+                                <div class="col-xs-6">
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <!-- Menu -->
-        <div class="menu">
-            <ul class="list">
-                <li class="header text-center">MENU</li>
-                <li>
-                    <a href="#">
-                        <i class="material-icons col-green">explore</i>
-                        <span>Historia</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="activo_fijo/galeria.php">
-                        <i class="material-icons col-blue">collections</i>
-                        <span>Galerias</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="activo_fijo/noticias.php">
-                        <i class="material-icons col-blue-grey">description</i>
-                        <span>Noticias</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="material-icons col-deep-orange">book</i>
-                        <span>Catalogo de libros</span>
-                    </a>
-                </li>
 
-                <li>
-                    <a href="#">
-                        <i class="material-icons col-deep-purple">event</i>
-                        <span>Proximos Eventos</span>
-                    </a>
-                </li>
-                <li class="header">Nosotros</li>
-                <li>
-                    <a href="activo_fijo/Contactenos.php">
-                        <i class="material-icons col-red">contacts</i>
-                        <span>Contactenos</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="activo_fijo/sugerencias.php">
-                        <i class="material-icons col-amber">mail</i>
-                        <span>Sugerencias</span>
-                    </a>
-                </li>
+            <?php
+        }
+        ?>
 
-            </ul>
-        </div>
-        <!-- #Menu -->
-        <!-- Footer -->
-        <div class="legal">
-            <div class="copyright">
-                &copy; 2017 <a href="#">UES FMP</a>.
-            </div>
+        <!-- Jquery Core Js -->
 
-        </div>
-        <!-- #Footer -->
-    </aside>
-    <!-- #END# Left Sidebar -->
-</section>
-            
-            
+        <!-- Bootstrap Core Js -->
+        <script src="plugins/bootstrap/js/bootstrap.js"></script>
 
-           
-        
-    
+        <!-- Select Plugin Js -->
+        <script src="plugins/bootstrap-select/js/bootstrap-select.js"></script>
 
-    <!-- Jquery Core Js -->
-    <script src="plugins/jquery/jquery.min.js"></script>
+        <!-- Slimscroll Plugin Js -->
+        <script src="plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
 
-    <!-- Bootstrap Core Js -->
-    <script src="plugins/bootstrap/js/bootstrap.js"></script>
+        <!-- Waves Effect Plugin Js -->
+        <script src="plugins/node-waves/waves.js"></script>
 
-    <!-- Select Plugin Js -->
-    <script src="plugins/bootstrap-select/js/bootstrap-select.js"></script>
+        <!-- Custom Js -->
+        <script src="js/admin.js"></script>
 
-    <!-- Slimscroll Plugin Js -->
-    <script src="plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+        <!-- Demo Js -->
+        <script src="js/demo.js"></script>
 
-    <!-- Waves Effect Plugin Js -->
-    <script src="plugins/node-waves/waves.js"></script>
 
-    <!-- Jquery CountTo Plugin Js -->
-    <script src="plugins/jquery-countto/jquery.countTo.js"></script>
+    </body>
 
-    <!-- Morris Plugin Js -->
-    <script src="plugins/raphael/raphael.min.js"></script>
-    <script src="plugins/morrisjs/morris.js"></script>
-
-    <!-- ChartJs -->
-    <script src="plugins/chartjs/Chart.bundle.js"></script>
-
-    <!-- Flot Charts Plugin Js -->
-    <script src="plugins/flot-charts/jquery.flot.js"></script>
-    <script src="plugins/flot-charts/jquery.flot.resize.js"></script>
-    <script src="plugins/flot-charts/jquery.flot.pie.js"></script>
-    <script src="plugins/flot-charts/jquery.flot.categories.js"></script>
-    <script src="plugins/flot-charts/jquery.flot.time.js"></script>
-
-    <!-- Sparkline Chart Plugin Js -->
-    <script src="plugins/jquery-sparkline/jquery.sparkline.js"></script>
-
-    <!-- Custom Js -->
-    <script src="js/admin.js"></script>
-    <script src="js/pages/index.js"></script>
-
-    <!-- Demo Js -->
-    <script src="js/demo.js"></script>
-</body>
-
-</html>
+</html>   
