c35930fc2192f3c4fcc0f394db81599236e0aa3d
95roma <romarioavr@gmail.com>
2018-01-21 23:06:39 -0600

    intendo agregar depre

--- activo_fijo/Lista.php
+++ activo_fijo/Lista.php
@@ -17,26 +17,26 @@
 include_once '../plantilla/cabecera.php';
 include_once '../plantilla/barraSuperior.php';
 include_once '../plantilla/barra_lateral_usuario.php';
-
 ?>
 <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
 <script src="jquery-3.1.1.min.js"></script>
 <script src="bootstrap/js/bootstrap.js"></script>
 <?php
-$con =new mysqli('localhost','root','root','instituciones_financieras');
-$datos=$con->query("select activo.estado,tipo_activo.nombre as aa, activo.precio,departamento.nombre, usuario.nombre as nombre_de_usuario, institucion.nombre AS
-nombre_institucion, encargado.nombre as encargado FROM
+// AGRAGAR CONTRASENA SI TIENEN CONTRA EL XAMPP
+$con = new mysqli('localhost', 'root', '', 'instituciones_financieras');
+$datos = $con->query("select activo.estado,tipo_activo.nombre as aa, activo.precio,departamento.nombre, usuario.nombre as nombre_de_usuario, institucion.nombre AS
+nombre_institucion, encargado.nombre as encargado,activo.id_activo as id FROM
 activo, usuario, departamento, institucion, tipo_activo, encargado
-WHERE tipo_activo.id_tipo=activo.id_tipo GROUP BY activo.id_activo  
+WHERE tipo_activo.id_tipo=activo.id_tipo GROUP BY activo.id_activo 
 ");
 ?>
-<form action="" method="post" class="formNatural" name="credito_personal" id="credito_personal" onsubmit="return validarTablas_cper()" enctype="multipart/form-data" >
+<!--<form action="" method="post" class="formNatural" name="credito_personal" id="credito_personal" onsubmit="return validarTablas_cper()" enctype="multipart/form-data" >-->
     <input type="hidden" id="pas_cp" name="pas_cp"/>
     <input type="hidden" id="n" name="n" value="1"/>
     <input type="radio" id="uno" checked="" style="display: none"/>
     <section class="content">
         <!--    INICIO DE DATOS-->
-        <div class="container-fluid">
+        <div class="container-fluid" id="contenido">
             <div class="row clearfix">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="card">
@@ -45,7 +45,7 @@ WHERE tipo_activo.id_tipo=activo.id_tipo GROUP BY activo.id_activo
                         </div>
                         <div class="body">
                             <div class="row clearfix">
-                                
+
 
 
 
@@ -53,7 +53,7 @@ WHERE tipo_activo.id_tipo=activo.id_tipo GROUP BY activo.id_activo
                                     <caption></caption>
                                     <thead>
                                     <th>Actvio</th>
-                                   
+
                                     <th>Departamento</th>
                                     <th>Precio</th>
                                     <th>Institucion</th>
@@ -61,71 +61,70 @@ WHERE tipo_activo.id_tipo=activo.id_tipo GROUP BY activo.id_activo
                                     <th>Acciones </th>  
                                     </thead>
                                     <tbody class="buscar">
-     				<?php while($fila=mysqli_fetch_array($datos)){?>
-						
-                        <tr>
-                        	
-                          
-                           <td><?php echo $fila['estado']; ?></td>
-                           
-                            <td><?php echo $fila['nombre']; ?></td>
-                              
-                              <td><?php echo $fila['precio'].' $' ; ?></td>
-                              
-                                    <td><?php echo $fila['nombre_institucion']; ?></td>
+                                        <?php while ($fila = mysqli_fetch_array($datos)) { ?>
 
-                                  <td><?php echo $fila['encargado']; ?></td>
-                                  <td 
-                                   <button class="btn btn-info" 
-                onclick="verDepreciacion('<?php echo $fila['nombre'] ?>','<?php echo $fila['precio'] ?>')"> 
-                <i class="fa fa-eye"></i>Depreciacion</td>
-                </button>
-                                      
-                            
-                            
-                            </tr>
-                            <?php
-						}
-							?>
-							
-                        </tbody>
+                                            <tr>
+
+
+                                                <td><?php echo $fila['estado']; ?></td>
+
+                                                <td><?php echo $fila['nombre']; ?></td>
+
+                                                <td><?php echo $fila['precio'] . ' $'; ?></td>
+
+                                                <td><?php echo $fila['nombre_institucion']; ?></td>
+
+                                                <td><?php echo $fila['encargado']; ?></td>
+                                                <td >
+                                                    <button class="btn btn-info" type="button" onclick="llamarPagina('<?php echo $fila['id']; ?>')"> 
+                                                        <i class="fa fa-eye"></i>Depreciacion
+                                                </button>
+                                                    </td>
+
+
+                                            </tr>
+                                            <?php
+                                        }
+                                        ?>
+
+                                    </tbody>
                                 </table>
-                                
+
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <!--    FIN DE DATOS-->
+        </div>
 
-           
 
-            
+
     </section>
-</form>
+<!--</form>-->
 
 <!-- Modal -->
 <div class="modal fade" id="mimodalejemplo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Ayuda a usuario</h4>
-      </div>
-      <div class="modal-body">
-        	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-       
-      </div>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Ayuda a usuario</h4>
+            </div>
+            <div class="modal-body">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 <datalist id="lista_personas_naturales">
     <?php
@@ -140,24 +139,11 @@ WHERE tipo_activo.id_tipo=activo.id_tipo GROUP BY activo.id_activo
 </datalist>
 
 
-	<script type="text/javascript" class="init">
-	
-
-$(document).ready(function() {
-	$('#example').DataTable();
-} );
-
+<script type="text/javascript" >
 function llamarPagina(id){
-	window.open("../activo_fijo/depreciacion.php?datos="+id, '_parent');
+    $("#contenido").load('ver_depreciacion.php?clienteinfo: "contenido"'});
+//	window.open("../activo_fijo/ver_depreciacion.php?datos="+id, '_parent');
 	}
 
-function llamarPaginaImprimir(id){
-	window.open("dar.php?iddatos="+id, '_parent');
-	}	
-	
-	
-	
-		
-		
-	
-	</script>
+    
+</script>
--- activo_fijo/ver_depreciacion.php
+++ activo_fijo/ver_depreciacion.php
@@ -1,49 +1,64 @@
+<?php
+$clienteInfo=$_REQUEST['clienteinfo'];
+echo $clienteInfo;
+?>
 <div class="row" >
     <form id="imprimir_depre" method="post" action="../reportesActivo/imp_depre.php" target="_blank">
-    <table id="no_imp">
-        <tbody>
-            <tr>
-                <td colspan="4" style="height:15px;">
-                    <div class="input-field col s12">
-                        <input type="text" id="ver_cod_depre" name="ver_cod_depre" placeholder="" minlength="8"  readonly=""   >
-                        <label for="textarea1" style="font-size:15px"><i class="fa fa-pencil-square-o"></i>C&oacutedigo</label>
-                    </div>
-                </td>
-            </tr>
-            <tr >
-                <td style="height:10px;"><div class="col m12">
-                        <div class="input-field">
-                            <i class="fa fa-calendar prefix" aria-hidden="true"></i>
-                            <label for="fecha_pub"  class="active" style="font-size:16px">Fecha Adquisición</label>
-                            <input type="text" name="ver_fecha_depre" placeholder="" id="ver_fecha_depre" readonly=""   >
+        <table id="no_imp">
+            <tbody>
+                <tr>
+                    <td colspan="4" style="height:15px;">
+                        <div class="input-field col s12">
+                            <input type="text" id="ver_cod_depre" name="ver_cod_depre" placeholder="" minlength="8"  readonly=""   >
+                            <label for="textarea1" style="font-size:15px"><i class="fa fa-pencil-square-o"></i>C&oacutedigo</label>
                         </div>
-                    </div></td>
-                <td style="height:10px;">
-                    <div class="input-field col m12">
-                        <i class="fa fa-usd prefix"></i> 
-                        <input type="number" name="ver_valor" placeholder="" min="0" step="any" id="ver_valor"  class="text-center validate" readonly="">
-                        <label for="precioUnitario" style="font-size:16px">Valor del Activo<small></small> </label>
-                    </div>
-                </td>
-            </tr>
-            
-        </tbody>
-    </table>
-   
-        
-    <table id="ver_depre_tab" class="text-center">
-        <caption>Depreciación </caption>
-        <thead>
-        <th class="text-center" >Año</th>
-        <th class="text-center">Valor del Activo</th>
-        <th class="text-center">Depreciación</th>
-        <th class="text-center">Valor Neto</th>
-        </thead>
-        <tbody></tbody>
-    </table>
-</form>
+                    </td>
+                </tr>
+                <tr >
+                    <td style="height:10px;"><div class="col m12">
+                            <div class="input-field">
+                                <i class="fa fa-calendar prefix" aria-hidden="true"></i>
+                                <label for="fecha_pub"  class="active" style="font-size:16px">Fecha Adquisición</label>
+                                <input type="text" name="ver_fecha_depre" placeholder="" id="ver_fecha_depre" readonly=""   >
+                            </div>
+                        </div></td>
+                    <td style="height:10px;">
+                        <div class="input-field col m12">
+                            <i class="fa fa-usd prefix"></i> 
+                            <input type="number" name="ver_valor" placeholder="" min="0" step="any" id="ver_valor"  class="text-center validate" readonly="">
+                            <label for="precioUnitario" style="font-size:16px">Valor del Activo<small></small> </label>
+                        </div>
+                    </td>
+                </tr>
+
+            </tbody>
+        </table>
+
+
+        <table id="ver_depre_tab" class="text-center">
+            <caption>Depreciación </caption>
+            <thead>
+            <th class="text-center" >Año</th>
+            <th class="text-center">Valor del Activo</th>
+            <th class="text-center">Depreciación</th>
+            <th class="text-center">Valor Neto</th>
+            </thead>
+            <tbody></tbody>
+        </table>
+
+        <button class="btn btn-info" type="button" onclick="llamarPaginaBack('')"> 
+            <i class="fa fa-eye"></i>Volver
+        </button>
+    </form>
 </div>
 
+<script type="text/javascript" >
+function llamarPaginaBack(id){
+    $("#contenido").load('lista.php');
+//	window.open("../activo_fijo/ver_depreciacion.php?datos="+id, '_parent');
+	}
 
+    
+</script>
 
 
